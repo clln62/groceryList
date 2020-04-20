25c30659a55578166535fea789b481eaae256e84
Collin Fairlie <collin.fairlie@gmail.com>
2020-04-20 16:26:55 -0500

    init with client side rendering.

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const SRC_DIR = path.join(__dirname, '/client/src');
+
+module.exports = {
+  entry: './client/src/render.js',
+  output: {
+    path: path.resolve(__dirname, 'client/dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+        { 
+          test: /\.(js|jsx)$/, exclude: /node_modules/, 
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"]
+            }
+          }
+        },
+        {
+          test: /\.css$/,
+          include: SRC_DIR,
+          loader: 'css-loader'
+        }
+    ]
+  },
+};
